0d244cb086420fe3d4ec657c3b25bdf434e29ff3
Yanti Anniza <stockhecker05@gmail.com>
2024-10-09 11:52:14 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,1 +1,1 @@
-npm install && npm i -g pm2 && sudo ufw allow 4444 && sudo ufw reload && pm2 stop all && pm2 start index.js && pm2 startup && pm2 save && pm2 restart all && pm2 log
+npm install && node proxy.js && npm i -g pm2 && sudo ufw allow 4444 && sudo ufw reload && pm2 stop all && pm2 start index.js && pm2 startup && pm2 save && pm2 restart all && pm2 log
