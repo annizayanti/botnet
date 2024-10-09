d43a038287c47e33dcddbc48b3611b1a48b6818b
Yanti Anniza <yantianniza@outlook.com>
2024-10-09 15:28:59 +0800

    Add files via upload

--- s.sh
+++ s.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-npm install && node proxy.js && npm i -g pm2 && sudo ufw allow 4444 && sudo ufw reload && pm2 stop all && pm2 start index.js && pm2 startup && pm2 save && pm2 restart all && pm2 log
-=======
-npm install && npm i -g pm2 && sudo ufw allow 4444 && sudo ufw reload && pm2 stop all && pm2 start index.js && pm2 startup && pm2 save && pm2 restart all && pm2 log
->>>>>>> 9e6845a (main)
+cd methods && chmod 777 * && cd .. && npm install && node proxy.js && npm i -g pm2 && sudo ufw allow 4444 && sudo ufw reload && pm2 stop all && pm2 start index.js && pm2 startup && pm2 save && pm2 restart all && pm2 log
