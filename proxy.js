1520740e052430a931ef458a99428a7c42c2b45c
Yanti Anniza <yantianniza@outlook.com>
2024-10-09 11:50:04 +0800

    Add files via upload

--- proxy.js
+++ proxy.js
@@ -1,35 +1,55 @@
-const axios = require('axios');
 const fs = require('fs');
 
-async function scrapeProxies() {
-  const proxySources = [
-'https://raw.githubusercontent.com/annizayanti/proxy/refs/heads/main/proxy.txt',
-'https://raw.githubusercontent.com/annizayanti/proxy/refs/heads/main/http_old.txt',
-'https://raw.githubusercontent.com/annizayanti/proxy/refs/heads/main/http.txt',
-'https://raw.githubusercontent.com/annizayanti/proxy/refs/heads/main/hproxy.txt',
-'https://raw.githubusercontent.com/annizayanti/proxyyy/refs/heads/main/proxy.txt',
-'https://raw.githubusercontent.com/annizayanti/proxyyy/refs/heads/main/proxyyy.txt',
-'https://raw.githubusercontent.com/annizayanti/proxyyy/refs/heads/main/proxyyyy.txt'
-  ];
+const proxies = [];
+const output_file = 'proxy.txt';
 
-  let proxies = [];
 
-    // Hapus file proxy.txt lama
-  if (fs.existsSync('proxy.txt')) {
-    fs.unlinkSync('proxy.txt');
-    console.log(`proxy.txt lama berhasil dihapus`);
-  }
-  
-  for (const source of proxySources) {
+
+const raw_proxy_sites = [
+"https://raw.githubusercontent.com/annizayanti/proxy/refs/heads/main/proxy.txt",
+"https://raw.githubusercontent.com/annizayanti/proxy/refs/heads/main/http_old.txt",
+"https://raw.githubusercontent.com/annizayanti/proxy/refs/heads/main/http.txt",
+"https://raw.githubusercontent.com/annizayanti/proxy/refs/heads/main/hproxy.txt",
+"https://raw.githubusercontent.com/annizayanti/proxyyy/refs/heads/main/proxy.txt",
+"https://raw.githubusercontent.com/annizayanti/proxyyy/refs/heads/main/proxyyy.txt",
+"https://raw.githubusercontent.com/annizayanti/proxyyy/refs/heads/main/proxyyyy.txt"
+];
+
+async function fetchProxies() {
+  for (const site of raw_proxy_sites) {
     try {
-      const response = await axios.get(source);
-      proxies = proxies.concat(response.data.split('\n'));
+      const response = await fetch(site);
+      if (response.ok) {
+//console.log(`success: ${site}`);
+        const data = await response.text();
+        const lines = data.split('\n');
+        for (const line of lines) {
+          if (line.includes(':')) {
+            const [ip, port] = line.split(':', 2);
+            proxies.push(`${ip}:${port}`);
+          }
+        }
+      } else {
+//console.log(`skip: ${site}`);
+      }
     } catch (error) {
-      console.log(`Error Scraping`);
+//console.error(`skip: ${site}`);
     }
   }
-
-  fs.writeFileSync('proxy.txt', proxies.join('\n'));
-  console.log(`Proxies successfully scraped and saved to proxy.txt`);
+if (fs.existsSync(output_file)) {
+  fs.unlinkSync(output_file);
+  console.log(`'${output_file}' telah dihapus.`);
 }
-module.exports = scrapeProxies;
+
+  fs.writeFileSync(output_file, proxies.join('\n'));
+  fs.readFile(output_file, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Gagal membaca file:', err);
+      return;
+    }
+    const proxies = data.trim().split('\n');
+    const totalProxies = proxies.length;
+    console.log(`success scraping ${totalProxies} proxy`);
+  });
+}
+fetchProxies();
